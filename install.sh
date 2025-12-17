39ae7f226bf744c65d7326fc15ea2f47aef82de7
Carlos Eduardo Ardigo <e.ardigo@gmail.com>
2025-12-16 21:44:44 -0300

    Adicionar verificação

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ echo "➡ Instalando homelab-alerts..."
 install -m 755 alert.sh "$BIN/alert.sh"
 install -m 755 check-updates.sh "$BIN/check-updates.sh"
 install -m 644 cron/check-updates /etc/cron.d/check-updates
+install -m 755 check-unattended-upgrades.sh /usr/local/bin/check-unattended-upgrades.sh
+install -m 644 cron/check-unattended-upgrades /etc/cron.d/check-unattended-upgrades
 
 echo "✔ Scripts e cron instalados"
 echo "ℹ Secrets esperados em $CONF/telegram.env"
